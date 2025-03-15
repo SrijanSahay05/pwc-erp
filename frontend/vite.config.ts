050787405ed8f524508f2678a9adcd09af3ea85d
Srijan Sahay <srijan05sahay@gmail.com>
2025-03-15 18:46:06 +0530

    Allowed Host added in Vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['srijansahay05.in'],
+    host: '0.0.0.0', // Ensure the server is accessible externally
+    port: 3000, // Ensure the port matches the one exposed in Dockerfile
+  },
 })
